f26719cee7f39c203f088c9ff4c112306b0705ba
pasan <111180840+SethmiDanoshi@users.noreply.github.com>
2025-04-07 18:59:01 +0530

    add backend setup

---
+++ src/main/java/com/petmate/petmatebackend/PetMateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.petmate.petmatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetMateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetMateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petmate/petmatebackend/PetMateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petmate.petmatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetMateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
